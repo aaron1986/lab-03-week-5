5afa07719a9ef90c4bdc01fbfa6d758d462edb8d
aaron1986 <aaronbrucesmith86@gmail.com>
2023-10-25 14:59:43 +0100

    gallery updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
 import './App.css'
-import Gallery from './components/Gallery'
+import Header from '/src/components/Header'
+import Gallery from '/src/components/Gallery'
+import Data from '/src/assets/data.json'
 
 
 function App() {
 
   return (
-    <Gallery />
-  )
+    <div>
+      <h1>Lab 03</h1>
+           <section id="grid">
+           <div id="image-grid">
+        {Data.map((item) => ( 
+          <div key={item._id}>
+            <img src={item.image_url} alt={item.title}/>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <p>Keyword: {item.keyword}</p>
+            <p>Horns: {item.horns}</p>
+          </div>
+        ))};
+        </div>
+      </section> 
+    </div> //end of top div
+  );
 }
 
 export default App
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,10 @@
-import Data from '/src/assets/data.json'
+import React from 'react';
+
 
 export default function Gallery() {
-    return Data.map((item)=> (
-        <Data 
-        key = {item._id}
-        title = {item.title}
-        imageUrl = {item.imageUrl}
-        description = {item.description}
-        />
-    ));
+  return (
+ <div>
+
+ </div>
+  );
 }
